497e5487acb435027690250d28ccc22eb1d1dd94
Koober97 <koober@Brians-MacBook-Pro.local>
2022-11-08 18:26:59 -0500

    create function to generate cards, must test

--- src/generate-html.js
+++ src/generate-html.js
@@ -9,3 +9,30 @@ const uniqueGetter = function (employee) {
         return `School: ${employee.school}`;
     }
   };
+
+  //creates the employee cards
+function generateCards(data) {
+    return data
+      .map((employee) => {
+        const { name, id, email } = employee;
+        const role = employee.getRole();
+        //get unique attribute html
+        const unique = uniqueGetter(employee);
+        return `
+        <div class="card m-3" style="width: 15rem">
+          <div class="card-header text-bg-primary">
+            <h5 class="card-title">${name}</h5>
+            <h6 class="card-text">${role}</h6>
+          </div>
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item">ID: ${id}</li>
+            <li class="list-group-item">
+              Email: <a href="mailto:${email}">${email}</a>
+            </li>
+            <li class="list-group-item">${unique}</li>
+          </ul>
+        </div>
+        `;
+      })
+      .join("");
+  }
