d8a5442b0aa1193298507af0464cad989590969a
Koober97 <koober@Brians-MacBook-Pro.local>
2022-11-08 18:30:25 -0500

    created function that houses the template

--- src/generate-html.js
+++ src/generate-html.js
@@ -36,3 +36,33 @@ function generateCards(data) {
       })
       .join("");
   }
+
+  module.exports = (employeeData) => {
+    const cards = generateCards(employeeData);
+    //the template function
+    return `
+   <!DOCTYPE html>
+  <html lang="en">
+    <head>
+      <meta charset="UTF-8" />
+      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <title>My Team Profile</title>
+      <link
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css"
+        rel="stylesheet"
+        integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx"
+        crossorigin="anonymous"
+      />
+    </head>
+    <body>
+      <header class="bg-danger d-flex justify-content-center">
+        <h1 class="text-light m-3">My Team</h1>
+      </header>
+      <main class="m-3 d-flex flex-wrap justify-content-center">
+        ${cards}
+      </main>
+    </body>
+  </html>
+   `;
+  };
